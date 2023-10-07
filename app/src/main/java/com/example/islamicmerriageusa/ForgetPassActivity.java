20954afbfbbcf5be3b7ead7efcd6e302ff32b122
alamingazi12 <47138908+alamingazi12@users.noreply.github.com>
2023-10-07 13:53:18 +0600

    update Push

---
+++ app/src/main/java/com/example/islamicmerriageusa/ForgetPassActivity.java
@@ -0,0 +1,15 @@
+package com.example.islamicmerriageusa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgetPassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_pass);
+
+    }
+}
