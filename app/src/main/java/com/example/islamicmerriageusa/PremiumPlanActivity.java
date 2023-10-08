8dc047cc445449141ba2000cccbcc8b757bbdce6
alamingazi12 <47138908+alamingazi12@users.noreply.github.com>
2023-10-08 13:22:48 +0600

    update Push

---
+++ app/src/main/java/com/example/islamicmerriageusa/PremiumPlanActivity.java
@@ -0,0 +1,14 @@
+package com.example.islamicmerriageusa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PremiumPlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_premium_plan);
+    }
+}
