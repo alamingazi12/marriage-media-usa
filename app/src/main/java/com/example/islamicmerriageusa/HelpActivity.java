32029727d162354bf4bcc87504715cab081b80de
alamingazi12 <47138908+alamingazi12@users.noreply.github.com>
2023-10-14 13:52:26 +0600

    update Push

--- app/src/main/java/com/example/islamicmerriageusa/HelpActivity.java
+++ app/src/main/java/com/example/islamicmerriageusa/HelpActivity.java
@@ -3,11 +3,11 @@ package com.example.islamicmerriageusa;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SearchActivity extends AppCompatActivity {
+public class HelpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.activity_help);
     }
 }
--- app/src/main/java/com/example/islamicmerriageusa/ProfileActivity.java
+++ app/src/main/java/com/example/islamicmerriageusa/ProfileActivity.java
@@ -1,13 +1,14 @@
 package com.example.islamicmerriageusa;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class SearchActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.activity_profile);
     }
 }
