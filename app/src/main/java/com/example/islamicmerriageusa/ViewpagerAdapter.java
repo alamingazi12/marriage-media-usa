dce377b538027de1b7fb78460cf1f9da3112790b
alamingazi12 <47138908+alamingazi12@users.noreply.github.com>
2023-10-15 11:18:30 +0600

    update Push

---
+++ app/src/main/java/com/example/islamicmerriageusa/ViewpagerAdapter.java
@@ -0,0 +1,49 @@
+package com.example.islamicmerriageusa;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager.widget.PagerAdapter;
+
+
+public class ViewpagerAdapter extends PagerAdapter {
+    public ViewpagerAdapter(Context context) {
+        this.context = context;
+    }
+
+    Context context;
+
+    int images[]={R.drawable.bordone,R.drawable.bord2,R.drawable.bordthree};
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull  View view, @NonNull  Object o) {
+        return view==(ConstraintLayout) o;
+    }
+
+    @NonNull
+
+    @Override
+    public Object instantiateItem(@NonNull  ViewGroup container, int position) {
+        LayoutInflater layoutInflater=(LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+       View view= layoutInflater.inflate(R.layout.slider_layout,container,false);
+          ImageView imageView=view.findViewById(R.id.slider_image);
+          imageView.setImageResource(images[position]);
+          container.addView(view);
+       return view;
+
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull  Object object) {
+        container.removeView((ConstraintLayout)object);
+    }
+}
--- app/src/main/java/com/example/islamicmerriageusa/WelcomeActivity.java
+++ app/src/main/java/com/example/islamicmerriageusa/WelcomeActivity.java
@@ -1,14 +1,73 @@
 package com.example.islamicmerriageusa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    ViewPager slider_viewPager;
+    Button next,back;
+    TextView[] dots;
+    LinearLayout mdotlinearlayout;
+   // ViewpagerAdapter viewpagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        slider_viewPager=findViewById(R.id.my_view_pager);
+        next=findViewById(R.id.button2);
+        back=findViewById(R.id.button);
+        mdotlinearlayout=findViewById(R.id.dotlayout);
+        ViewpagerAdapter viewpagerAdapter=new ViewpagerAdapter(WelcomeActivity.this);
+        slider_viewPager.setAdapter(viewpagerAdapter);
+        setUpIndicator(0);
+        slider_viewPager.addOnPageChangeListener(pageChangeListener);
+    }
+
+    public void setUpIndicator(int position){
+        dots=new TextView[3];
+        mdotlinearlayout.removeAllViews();
+        for(int i=0;i<dots.length;i++){
+            dots[i]=new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226"));
+            dots[i].setTextSize(40);
+            dots[i].setTextColor(getResources().getColor(R.color.teal_200));
+            mdotlinearlayout.addView(dots[i]);
+        }
+        dots[position].setTextColor(getResources().getColor(R.color.black));
+    }
+    ViewPager.OnPageChangeListener pageChangeListener=new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            setUpIndicator(position);
+            if(position>0){
+               // back.setVisibility(View.VISIBLE);
+            }else {
+               // back.setVisibility(View.INVISIBLE);
+            }
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
+        }
+    };
+    private int getItem(int i){
+        return slider_viewPager.getCurrentItem()+i;
     }
 }
