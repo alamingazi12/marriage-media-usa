0c1cfe552c52686e3d048d240fb96175e2f84062
alamingazi12 <47138908+alamingazi12@users.noreply.github.com>
2023-10-30 16:58:04 +0600

    update Push

---
+++ app/src/main/java/com/example/islamicmerriageusa/SingleProductActivity.java
@@ -0,0 +1,64 @@
+package com.example.islamicmerriageusa;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class SingleProductActivity extends AppCompatActivity {
+    ViewPager product_viewPager;
+    TextView[] dots;
+    LinearLayout mdotlinearlayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_product);
+        product_viewPager=findViewById(R.id.product_viewpager);
+        mdotlinearlayout=findViewById(R.id.p_dot_layout);
+        product_viewPager.setAdapter(new ProductPagerAdapter(SingleProductActivity.this));
+        setUpIndicator(0);
+        product_viewPager.addOnPageChangeListener(pageChangeListener);
+    }
+
+    ViewPager.OnPageChangeListener pageChangeListener=new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            setUpIndicator(position);
+            if(position>0){
+                // back.setVisibility(View.VISIBLE);
+            }else {
+                // back.setVisibility(View.INVISIBLE);
+            }
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
+        }
+    };
+    private int getItem(int i){
+        return product_viewPager.getCurrentItem()+i;
+    }
+
+    public void setUpIndicator(int position){
+        dots=new TextView[3];
+        mdotlinearlayout.removeAllViews();
+        for(int i=0;i<dots.length;i++){
+            dots[i]=new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226"));
+            dots[i].setTextSize(40);
+            dots[i].setTextColor(getResources().getColor(R.color.teal_200));
+            mdotlinearlayout.addView(dots[i]);
+        }
+        dots[position].setTextColor(getResources().getColor(R.color.black));
+    }
+}
