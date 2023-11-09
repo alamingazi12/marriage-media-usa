d6b233772e50715ee3aa53cf3927d555dd746bef
alamingazi12 <47138908+alamingazi12@users.noreply.github.com>
2023-11-09 11:21:52 +0600

    update Push

---
+++ app/src/main/java/com/example/islamicmerriageusa/SearchBiodataActivity.java
@@ -0,0 +1,14 @@
+package com.example.islamicmerriageusa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchBiodataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_biodata);
+    }
+}
