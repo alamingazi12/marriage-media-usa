4333777ac014e6823fbab53196ab2c8df0ff57f3
alamingazi12 <47138908+alamingazi12@users.noreply.github.com>
2023-11-11 14:02:04 +0600

    update Push

---
+++ app/src/main/java/com/example/islamicmerriageusa/SearchCommunityActivity.java
@@ -0,0 +1,44 @@
+package com.example.islamicmerriageusa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import static com.example.islamicmerriageusa.R.array.*;
+
+public class SearchCommunityActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    String[] country = { "Sponsored", "Most Reviewed"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_community);
+        //Getting the instance of Spinner and applying OnItemSelectedListener on it
+        Spinner spin = (Spinner) findViewById(R.id.spinner);
+        spin.setOnItemSelectedListener(this);
+
+        //Creating the ArrayAdapter instance having the country list
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,country);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(
+                this,
+                my_arr,
+                R.layout.my_spinner);
+        spin.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(), country[position] , Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
