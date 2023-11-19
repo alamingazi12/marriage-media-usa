b03c0a012414c4b33ef9d4a0d692dffda09b4d7c
alamingazi12 <47138908+alamingazi12@users.noreply.github.com>
2023-11-19 12:33:21 +0600

    update Push

--- app/src/main/java/com/example/islamicmerriageusa/SearchCommunityActivity.java
+++ app/src/main/java/com/example/islamicmerriageusa/SearchCommunityActivity.java
@@ -3,7 +3,6 @@ package com.example.islamicmerriageusa;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
